9835d0c25a36fcbd3077ff0a97bce3e0a845f8f8
nico <nicorg2529@gmail.com>
2018-09-16 07:56:03 +0000

    Convert to lineage-16.0

--- msm8916.mk
+++ msm8916.mk
@@ -294,6 +294,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 $(call inherit-product-if-exists, vendor/motorola/msm8916-common/msm8916-common-vendor.mk)
 $(call inherit-product-if-exists, vendor/motorola/msm8916-common/msm8916-common-vendor-ims-blobs.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 include device/motorola/msm8916-common/vendor-prop.mk
