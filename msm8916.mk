d772dc22ece9f8ee4f662cc0f549b5b25f7878ae
nico <nicorg2529@gmail.com>
2018-10-06 10:30:12 +0000

    Build textclassifier package

--- msm8916.mk
+++ msm8916.mk
@@ -255,6 +255,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
