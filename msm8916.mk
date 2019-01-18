de4a8d8de86ce916a25cacc11a0ace8401b9a07a
Paul Keith <javelinanddart@gmail.com>
2019-01-18 23:36:25 +0100

    msm8916-common: Build livedisplay 2.0 service
    Change-Id: Ia96dfe341d07c9f79b1ab5bd19af0e32bee4b4ed

--- msm8916.mk
+++ msm8916.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-	vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm
 
 # Media
 PRODUCT_COPY_FILES += \
