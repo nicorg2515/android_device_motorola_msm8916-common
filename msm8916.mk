77e8e538bc6aad673f93d3a56824b9786dce2253
Nico <nicorg2529@gmail.com>
2019-10-13 12:05:11 +0000

    msm8916-common: Remove msm8939 thermal HAL

--- msm8916.mk
+++ msm8916.mk
@@ -296,9 +296,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.1-service.msm8939
-
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
