bc0f0b2616d856929f7105fc71fb3f77eebc0afd
Nico <nicorg2529@gmail.com>
2019-10-20 15:55:11 +0000

    msm8916-common: Update build prop script for 10
    * Update build.prop location to /system/system/build.prop
    * Add *.system.* properties

--- prebuilt/bin/update_build_prop.sh
+++ prebuilt/bin/update_build_prop.sh
@@ -9,8 +9,8 @@ update_prop() {
     #backup orginial prop file
     mv $target $backup
 
-    #write header, update and then original prop
-    printf "# %s\n" "$update" | cat - $update $backup > $target
+    #write original prop, header and then update prop
+    printf "# %s\n" "$update" | cat $backup - $update > $target
 
     unset target update backup
 }
@@ -19,19 +19,19 @@ update_prop() {
 sku=$(getprop ro.boot.hardware.sku)
 
 if [ "$sku" = "XT1556" ]; then
-    update_prop "/system/build.prop" "/tmp/install/bin/system_XT1556.prop"
+    update_prop "/system/system/build.prop" "/tmp/install/bin/system_XT1556.prop"
     update_prop "/vendor/build.prop" "/tmp/install/bin/vendor_XT1556.prop"
 elif [ "$sku" = "XT1557" ]; then
-    update_prop "/system/build.prop" "/tmp/install/bin/system_XT1557.prop"
+    update_prop "/system/system/build.prop" "/tmp/install/bin/system_XT1557.prop"
     update_prop "/vendor/build.prop" "/tmp/install/bin/vendor_XT1557.prop"
 fi
 
 unset sku
 
 #fix permission and context
-chmod 644 "/system/build.prop"
+chmod 644 "/system/system/build.prop"
 chmod 644 "/vendor/build.prop"
-chcon u:object_r:system_file:s0 "/system/build.prop"
+chcon u:object_r:system_file:s0 "/system/system/build.prop"
 chcon u:object_r:vendor_file:s0  "/vendor/build.prop"
 
 exit 0
