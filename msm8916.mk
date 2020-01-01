a6827db141718d1fc756001a41d80aee574e84de
Nico <nicorg2529@gmail.com>
2020-01-01 12:11:59 +0000

    msm8916-common: Use more TARGET_COPY_OUT_SYSTEM

--- msm8916.mk
+++ msm8916.mk
@@ -202,7 +202,7 @@ PRODUCT_BOOT_JARS += \
     ims-ext-common_system
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # IRSC
 PRODUCT_COPY_FILES += \
